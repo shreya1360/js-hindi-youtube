820abe7289acff6c56a8b24e51abaeead6e23ff5
Shreya Mishra <126142868+shreya1360@users.noreply.github.com>
2025-10-16 05:55:18 +0000

    fourth var scopes commit

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,51 @@
+
+// var c =300;
+let a = 400;
+if(true){
+  let a = 10
+const b = 20
+// c=30 
+// console.log( "inner:", a) 
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one(){
+//   const username ="hitesh"
+//   function two(){
+//     const website ="youtube"
+//     console.log(username);
+//   }
+  // console.log(website);
+  // two();
+// }
+
+// one();
+
+
+if(true){
+  const username ="ram"
+  if(username==="ram"){
+const website ="youtube"
+// console.log(username + " " + website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+
+// intresting
+
+addone(5);
+console.log(addone(5))
+
+function addone(num){
+  return num +1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+  return num + 2
+}
