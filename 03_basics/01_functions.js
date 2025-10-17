c399810f122491fecf516cfe3b8d5c5f7ae6e4b6
Shreya Mishra <126142868+shreya1360@users.noreply.github.com>
2025-10-17 01:52:24 +0000

    Fifth commit function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,38 @@
+
+
+function sayMyName(){
+    console.log("H");
+console.log("T");
+console.log("I");
+
+}
+
+ sayMyName()
+
+
+//  function addTwoNumbers(number1, number2){
+//     console.log(number1 +  number2);
+    
+//  }
+//  addTwoNumbers(3,'a');
+
+function addTwoNumbers(number1 , number2){
+//   let result = number1 + number2  
+//   console.log("ram");
+//   return result
+
+  // agar function me ek bar kuchh return kar diye toh uske bad kuchh bhi execute nahi hoga
+//   console.log("Shreya");
+
+//   easy way to return
+return number1 + number2
+  
+
+}
+const result = addTwoNumbers(3,5)
+
+console.log("result:", result);
+
+
+
+
