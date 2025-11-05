9d6d2e19555ec0b30c78f63659c05578f97f056f
Shreya Mishra <126142868+shreya1360@users.noreply.github.com>
2025-11-05 01:44:32 +0000

    control_flow_01

---
+++ 04_control_flow/one.js
@@ -0,0 +1,33 @@
+
+if(2=="2"){
+    console.log("executed");
+}
+const temprature =411;
+if(temprature <50){
+    console.log("less than 50");
+    
+}else{
+    console.log("temprature is greater than 50");
+}
+
+const score = 200
+if(score>100){
+  const power = "fly"
+  console.log(`user power: ${power}`);
+    
+}
+// console.log(`user power: ${power}`);
+
+const isUserloggedIn  = true
+ const debitCard = true
+ if(isUserloggedIn && debitCard && 22==3){
+    console.log("allow to buy course");
+ }else{
+    console.log("dont allow");
+    
+ }
+
+ if(isUserloggedIn || debitCard){
+    console.log("user logged in");
+    
+ }
